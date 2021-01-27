3b81afa05f356d6f4654076592ab4c0bf97dd9b2
dmdj-11-10 <fancy.5293@gamil.com>
2021-01-27 21:53:13 +0900

    Chapter #2.2 Clear!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react";
 
-function Food(props) {
+function Hyewon({name, picture}) {
   return (
-  <h1>{props.fav}가 좋아..!!</h1>
+    <div>
+      <h2>{name} Love</h2>
+      <img src={picture} alt={name+" img"} />
+    </div>
   )
 }
+
+const iLoveHye = [
+  {
+    name: "hyewon",
+    image: "https://lh3.googleusercontent.com/proxy/61tV8oBQlASyzg0O-pkEMu70Qz5w4ZGlzeZEF3XMk-nJowpifrOqx7J4_4_fwy5IPCCqFvro7cBlY7OFonIsLbWreYIy7ie7a441Ilxo5lUbioTUWyfpkNlSfQYmZcqptLyIVl3uZ6HM0yG5xJ_Y60R_Um1ZLMfh7Zqpk-9t5D7wcPs0j8kY9KWumgwXlVu8YJz1DXqMLha1N6qZILxlWkNtyM_SQfbqev7_hSp8HlRDGPmrT36-xFZySuF6P7qiOENm8hr6IErp9oS5zb6JTYHpZYXpMnpmZyQGUWMfNA"
+  },
+  {
+    name: "Love",
+    image: "https://adventuresoftheordinaryblog.files.wordpress.com/2020/06/love-technology-style.jpg"
+  }
+];
 
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="혜원이" />
-      <Food fav="진짜로 혜원이" />
-      <Food fav="엄청 혜원이" />
-      <Food fav="커여운 혜원이" />
+      {iLoveHye.map(love => (
+        <Hyewon name={love.name} picture={love.image} />
+      ))}
     </div>
   );
 }
