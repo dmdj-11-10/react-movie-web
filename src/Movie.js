3763fec60f1ee687d433f181c28b0f700e3b62ab
dmdj-11-10 <fancy.5293@gamil.com>
2021-01-30 00:23:53 +0900

    Chapter #4.5 Clear!

--- src/Movie.js
+++ src/Movie.js
@@ -17,6 +17,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
           ))}
         </ul>
         <p className="movie-summary">{summary}</p>
+        {/* <p className="movie-summary">{summary.slice(0, 180)}</p> : slice the Array*/}
       </div>
     </div>
   );
