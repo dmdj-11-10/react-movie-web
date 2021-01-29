c34164d09d28018d4e185db6c612b52c2df55fdb
dmdj-11-10 <fancy.5293@gamil.com>
2021-01-29 15:48:42 +0900

    Chapter #3.0 Clear!

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const iLoveHye = [
-  {
-    id: 1,
-    name: "hyewon",
-    image: "https://lh3.googleusercontent.com/proxy/61tV8oBQlASyzg0O-pkEMu70Qz5w4ZGlzeZEF3XMk-nJowpifrOqx7J4_4_fwy5IPCCqFvro7cBlY7OFonIsLbWreYIy7ie7a441Ilxo5lUbioTUWyfpkNlSfQYmZcqptLyIVl3uZ6HM0yG5xJ_Y60R_Um1ZLMfh7Zqpk-9t5D7wcPs0j8kY9KWumgwXlVu8YJz1DXqMLha1N6qZILxlWkNtyM_SQfbqev7_hSp8HlRDGPmrT36-xFZySuF6P7qiOENm8hr6IErp9oS5zb6JTYHpZYXpMnpmZyQGUWMfNA",
-    rating: 100
-  },
-  {
-    id: 2,
-    name: "Love",
-    image: "https://adventuresoftheordinaryblog.files.wordpress.com/2020/06/love-technology-style.jpg",
-    rating: 101
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  // state is Object and inner Data and data is Change.
+  
+  plus = () => {
+    console.log("plus");
+  };  // plus Function
+
+  minus = () => {
+    console.log("minus");
+  }; // minus Function
+
+  render() {
+    return (
+      <div>
+        <h1>The Number is: {this.state.count}</h1>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
-];
-
-function Hyewon({name, picture, rating}) {
-  return (
-    <div>
-      <h2>{name} Love</h2>
-      <p>{rating}</p>
-      <img src={picture} alt={name+" img"} />
-    </div>
-  )
-}
-
-Hyewon.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {iLoveHye.map(love => (
-            <Hyewon
-              key={love.id}
-              name={love.name}
-              picture={love.image}
-              rating={love.rating}
-            />
-          )
-        )
-      }
-    </div>
-  );
 }
 
 export default App;
